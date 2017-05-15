8ca4b6657165b9990ff7a6487235247434414aea
quentin0m <quentin.mathias64@gmail.com>
2017-05-15 20:02:45 +0200

    cleaning

--- ROBO/app/src/main/java/com/example/superprojet/robobo2/RoboboApp.java
+++ ROBO/app/src/main/java/com/example/superprojet/robobo2/RoboboApp.java
@@ -30,14 +30,11 @@ import java.util.concurrent.Callable;
  */
 
 /**
- * Classe séparée pour gérer les actions de robobo.
- * pour l'instant, il faut que RoboboManager (et textview mais c'est un détail) soit public dans MainActivity
+ * class with the sole purpose of lowering the tilt of the panel so that ROBOBO may be stored in its
+ * box
  */
 
 public class RoboboApp implements Runnable {
-
-    int leftVelocity = 0;
-    int rightVelocity = 0;
 
 
     private DefaultRobMovementModule bouger;
@@ -49,33 +46,6 @@ public class RoboboApp implements Runnable {
 
     private Callable<Integer> bhv;
 
-
-    public RoboboApp(RoboboManager roboboManager, MainActivity m, Callable<Integer> bhv) {
-        this.bhv = bhv;
-
-        ma = m;
-
-        ma.roboboManager = roboboManager;
-
-
-
-        try {
-            bouger = ma.roboboManager.getModuleInstance(DefaultRobMovementModule.class);
-
-            //emo = ma.roboboManager.getModuleInstance((DefaultEmotionModule.class));
-            move = ma.roboboManager.getModuleInstance((IRobMovementModule.class));
-
-            robModule = ma.roboboManager.getModuleInstance(BluetoothRobInterfaceModule.class);
-            //get the instance of the ROB interface
-            this.rob = robModule.getRobInterface();
-        }
-        catch(ModuleNotFoundException ex) {
-            //ma.tv.setText("erreur");
-            Log.d("RoboboApp", "erreur");
-            Log.e("ROBOBO-APP", "Module not found: "+ex.getMessage());
-        }
-
-    }
 
     public RoboboApp(IRob iRob, IRobMovementModule m, Callable<Integer> bhv) {
         this.bhv = bhv;
@@ -97,14 +67,6 @@ public class RoboboApp implements Runnable {
         }
 
 
-//        try {
-//            bhv.call();
-//            carre();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-        //actionSequence();
 
         try {
             move.moveTilt(6, 26);
@@ -114,202 +76,8 @@ public class RoboboApp implements Runnable {
 
     }
 
-    public void actionSequence(){
-
-        try{
-            //bouger.startup(ma.roboboManager);
-            // Le listener qui permet de savoir les statuts de ROBOBO.
-            // pour l'instant seuls les moteurs sont écoutés
-            rob.addRobStatusListener(new IRobStatusListener() {
-                @Override
-                public void statusMotorsMT(MotorStatus left, MotorStatus right) {
-                    RoboboApp.this.leftVelocity = left.getAngularVelocity();
-                    RoboboApp.this.rightVelocity = right.getAngularVelocity();
 
 
-
-                }
-
-                @Override
-                public void statusMotorPan(MotorStatus status) {
-
-                }
-
-                @Override
-                public void statusMotorTilt(MotorStatus status) {
-                    if(status.getVariationAngle()!=27){
-                        status.setVariationAngle(27);
-                        status.setAngularVelocity(6);
-                    }
-
-
-
-                }
-
-                @Override
-                public void statusGaps(Collection<GapStatus> gaps) {
-
-                }
-
-                @Override
-                public void statusFalls(Collection<FallStatus> fall) {
-
-                }
-
-                @Override
-                public void statusIRSensorStatus(Collection<IRSensorStatus> irSensorStatus) {
-
-                }
-
-                @Override
-                public void statusBattery(BatteryStatus battery) {
-
-                }
-
-                @Override
-                public void statusWallConnectionStatus(WallConnectionStatus wallConnectionStatus) {
-
-                }
-
-                @Override
-                public void robCommunicationError(InternalErrorException ex) {
-
-                }
-            });
-
-
-
-            rob.setRobStatusPeriod(50); // les statuts du robot sont vérifiés toutes les 50ms
-            rob.setOperationMode((byte)1);
-            rob.moveMT(MoveMTMode.FORWARD_FORWARD,80,2000,80,2000); // équivaut à un tour d'environ pi/2
-
-            Thread.sleep(200L); // on attend un peu pour que le listener ait mis à jour les statuts
-            move.moveTilt(6,27);
-
-
-            while(rightVelocity!=0 || leftVelocity!=0){
-                Thread.sleep(10L);// on attend que le robot ait terminé son action
-            }
-            //rob.moveMT(MoveMTMode.REVERSE_REVERSE,80,360,80,360);
-            Color c = new Color(160,0,0); // couleur à mettre en parametre pour les LED
-            rob.movePan(1000,800);
-            for(int i=1;i<=9;i++){
-                // setLEDColor s'occupe d'une LED à la fois
-                // pour le numéro des LED voir https://bitbucket.org/mytechia/robobo-framework/wiki/using-rob-interface
-                rob.setLEDColor(i, c);
-            }
-
-            /*if(!rob.getLastStatusMotors().isEmpty())
-            ma.tv.setText(rob.getLastStatusMotors().get(0).getAngularVelocity());*/
-
-        }catch (Exception e){
-            //ma.tv.setText(e.getMessage());
-            Log.d("actionSequence", e.getMessage());
-        }
-
-    }
-
-    public void carre() throws InternalErrorException, InterruptedException {
-        rob.addRobStatusListener(new IRobStatusListener() {
-            @Override
-            public void statusMotorsMT(MotorStatus left, MotorStatus right) {
-                RoboboApp.this.leftVelocity = left.getAngularVelocity();
-                RoboboApp.this.rightVelocity = right.getAngularVelocity();
-
-
-
-            }
-
-            @Override
-            public void statusMotorPan(MotorStatus status) {
-
-            }
-
-            @Override
-            public void statusMotorTilt(MotorStatus status) {
-                if(status.getVariationAngle()!=27){
-                    status.setVariationAngle(27);
-                    status.setAngularVelocity(6);
-                }
-
-
-
-            }
-
-            @Override
-            public void statusGaps(Collection<GapStatus> gaps) {
-
-            }
-
-            @Override
-            public void statusFalls(Collection<FallStatus> fall) {
-
-            }
-
-            @Override
-            public void statusIRSensorStatus(Collection<IRSensorStatus> irSensorStatus) {
-
-            }
-
-            @Override
-            public void statusBattery(BatteryStatus battery) {
-
-            }
-
-            @Override
-            public void statusWallConnectionStatus(WallConnectionStatus wallConnectionStatus) {
-
-            }
-
-            @Override
-            public void robCommunicationError(InternalErrorException ex) {
-
-            }
-        });
-        rob.setOperationMode((byte)1);
-
-        //for(int i=0;i<4;i++) {
-            rob.moveMT(MoveMTMode.FORWARD_FORWARD,80,2000,80,2000);
-            Thread.sleep(200L);
-            while(rightVelocity!=0 || leftVelocity!=0){
-                Thread.sleep(10L);// on attend que le robot ait terminé son action
-            }            rob.moveMT(MoveMTMode.FORWARD_REVERSE, 80, 240, 80, 240);
-            Thread.sleep(200L);
-            while(rightVelocity!=0 || leftVelocity!=0){
-                Thread.sleep(10L);// on attend que le robot ait terminé son action
-            }
-        rob.moveMT(MoveMTMode.FORWARD_FORWARD,80,2000,80,2000);
-        Thread.sleep(200L);
-        while(rightVelocity!=0 || leftVelocity!=0){
-            Thread.sleep(10L);// on attend que le robot ait terminé son action
-        }            rob.moveMT(MoveMTMode.FORWARD_REVERSE, 80, 240, 80, 240);
-        Thread.sleep(200L);
-        while(rightVelocity!=0 || leftVelocity!=0){
-            Thread.sleep(10L);// on attend que le robot ait terminé son action
-        }
-        rob.moveMT(MoveMTMode.FORWARD_FORWARD,80,2000,80,2000);
-        Thread.sleep(200L);
-        while(rightVelocity!=0 || leftVelocity!=0){
-            Thread.sleep(10L);// on attend que le robot ait terminé son action
-        }            rob.moveMT(MoveMTMode.FORWARD_REVERSE, 80, 240, 80, 240);
-        Thread.sleep(200L);
-        while(rightVelocity!=0 || leftVelocity!=0){
-            Thread.sleep(10L);// on attend que le robot ait terminé son action
-        }
-        rob.moveMT(MoveMTMode.FORWARD_FORWARD,80,2000,80,2000);
-        Thread.sleep(200L);
-        while(rightVelocity!=0 || leftVelocity!=0){
-            Thread.sleep(10L);// on attend que le robot ait terminé son action
-        }            rob.moveMT(MoveMTMode.FORWARD_REVERSE, 80, 240, 80, 240);
-        Thread.sleep(200L);
-        while(rightVelocity!=0 || leftVelocity!=0){
-            Thread.sleep(10L);// on attend que le robot ait terminé son action
-        }
-       // }
-    }
-    public void executeCommand (RoboboDNA rDNA)  {
-        rDNA.exec();
-    }
 }
 
 
